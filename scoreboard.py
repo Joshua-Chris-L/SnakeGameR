256cf64ac9927e125f857e6aa8aef9ca787eeda7
Joshua Christopher <joshchrislight@gmail.com>
2025-03-03 08:16:06 +0100

    secound

--- scoreboard.py
+++ scoreboard.py
@@ -9,20 +9,30 @@ class Scoreboard(Turtle):
         self.score =  0
         self.color("white")
         self.penup()
+        with open("my_file.txt", mode="r") as file:
+            contents = file.read()
+        self.high_score = int(contents)
         self.goto(0, 260)
         self.update_scoreboard()
         self.hideturtle()
         
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT , font= FONT )
+        self.clear()
+        self.write(f"Score: {self.score}  High_Score: {self.high_score}", align=ALIGNMENT , font= FONT )
     
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("my_file.txt", mode="w") as file:
+                file.write(self.high_score)
+        self.score = 0
+        
+    def increase_score(self):
+        self.score += 1 
+        self.update_scoreboard()
+        
+    """ 
     def game_over(self):
         self.goto(0,0)
         self.write("GAME OVER", align=ALIGNMENT , font= FONT )
-        
-    
-    def increase_score(self):
-        self.score += 1 
-        self.clear()
-        self.update_scoreboard()
-        
+    """
--- snake.py
+++ snake.py
@@ -27,6 +27,13 @@ class Snake:
     def extend(self):
         #Add new snake lenght to the last one
         self.add_segment(self.segments[-1].position())
+    
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(9000, 9000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
         
     def move(self):
         for seg_num in range(len(self.segments)-1, 0, -1):
